380e9e4dcb6e5394a942e4909bf3238fbda67d26
Dust C <137210879+Micael-Dombroski@users.noreply.github.com>
2025-04-19 18:58:19 -0300

    Bugfix CORS

--- santander-dev-week-2025/src/main/java/me/dio/Application.java
+++ santander-dev-week-2025/src/main/java/me/dio/Application.java
@@ -1,9 +1,12 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 @EntityScan("me.dio.domain.model")
 public class Application {
